dca2fa6cec0b332a43115578aa9f2ace2900291e
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2022-02-09 22:36:53 +0100

    Reads old/current members/csv, removes non fitting rows, does not add new members

--- checkSheets.py
+++ checkSheets.py
@@ -1,22 +1,33 @@
 import csv
 
-file = open("sheets/Members_Bank_Details.csv", encoding='utf-8-sig')
-csvReader = csv.reader(file)
-headerCurrent = next(csvReader)
-
 rowsCurrent = []
-for row in csvReader:
-    rowsCurrent.append(row)
-
-file.close()
-
-file = open("sheets/Rabobank_Incasso_Adresboek.csv", encoding='utf-8-sig')
-csvReader = csv.reader(file)
-headerOld = next(csvReader)
-
 rowsOld = []
-for row in csvReader:
-    rowsOld.append(row)
+headerOld = []
+headerCurrent = []
 
-file.close()
+with open("sheets/Members_Bank_Details.csv", encoding='utf-8-sig') as file:
+    csvReader = csv.reader(file)
+    headerCurrent = next(csvReader)
 
+    for row in csvReader:
+        rowsCurrent.append(row)
+
+with open("sheets/Rabobank_Incasso_Adresboek.csv", encoding='utf-8-sig') as file:
+    csvReader = csv.reader(file)
+    headerOld = next(csvReader)
+
+    for row in csvReader:
+        rowsOld.append(row)
+
+    for row in rowsOld[:]:
+        rowPresent = False
+        for checkRow in rowsCurrent:
+            if row[3].replace(" ", "") == checkRow[2].replace(" ", ""):
+                rowPresent = True
+        if not rowPresent:
+            rowsOld.remove(row)
+
+with open('sheets/Checked_Rabobank_Incasso.csv', 'w') as file:
+    write = csv.writer(file)
+    write.writerow(headerOld)
+    write.writerows(rowsOld)
